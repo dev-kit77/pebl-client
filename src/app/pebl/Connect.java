a75aee5c5e76e9d197e291e06bff45d74af641e2
YEEET214 <younisanees817@gmail.com>
2025-03-26 17:03:31 +0000

    added auth method which gets and sets the auth value in the config file

--- src/app/pebl/Connect.java
+++ src/app/pebl/Connect.java
@@ -7,6 +7,7 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONValue;
 import java.net.URI;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.File;
 import java.io.IOException;
@@ -19,18 +20,107 @@ import java.net.http.HttpResponse.BodyHandlers;
 
 
 public class Connect {
+    private HttpClient client;
+    private File config;
     private String auth;
-    private String path;
     public Connect() throws IOException{
-        File file = new File(".");
-        path = file.getCanonicalPath();
-        
+        client = HttpClient.newHttpClient(); //The client
+        auth("");
     }
 
-    //TODO add different functions (or reuse this one) to get data on users and another for getting posts.
     /**
-     * Method to test connection to server
+     * Updates the value of auth in the .pebl.cfg file
+     * @param newAuth new authentication token to update the config file. Set this to empty string to get the value from the config file or to set the auth token to empty string.
+     * 
      */
+    private void auth(String newAuth){
+
+        config = new File(new File("./.pebl.cfg").getCanonicalPath()); //teh config file
+        //Create the config file if it doesnt exist
+        if (!config.exists()) {
+            config.createNewFile();
+        }
+        FileInputStream file = null;
+        try {
+            file = new FileInputStream(config.getAbsolutePath());
+            Properties prop = new Properties();
+            prop.load(file);
+            if (!newAuth.equals("")) {
+                prop.setProperty("auth", newAuth);
+            }
+            else {
+
+                auth = prop.getProperty("auth", "");
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException e) {
+            System.out.println("IOException");
+        } 
+         finally {
+            if (file != null) {
+                try {
+                    file.close();
+                } 
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        if (auth.equals("")) {
+            request("register", new JSONObject());
+        }
+    }
+
+    /**
+     * Method to handle all http requests, just enter from the options []
+     * @param type where you are sending the request to
+     * @param body the body
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    public void request(String type, JSONObject body) throws IOException, InterruptedException{
+        HttpRequest request;
+        HttpResponse<String> response;
+        JSONObject authFull;
+        switch (type) {
+
+            case "register":
+                request = HttpRequest.newBuilder()
+                .uri(URI.create(api+type))
+                .POST(HttpRequest.BodyPublishers.ofString(body.toJSONString()))
+                .header("Content-Type", "application/json")
+                .build();
+
+                response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                System.out.println(response.statusCode());
+                System.out.println(response.body());
+                authFull = (JSONObject)JSONValue.parse(response.body()); //parse the response as JSON
+                auth = (String)authFull.get("token"); // update the auth token
+                authFull = null; //remove the JSON object
+
+                break;
+
+            case "auth":
+                request = HttpRequest.newBuilder()
+                .uri(URI.create(api+type))
+                .POST(HttpRequest.BodyPublishers.ofString(body.toString()))
+                .header("Content-Type", "application/json")
+                .build();
+
+                response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                System.out.println(response.statusCode());
+                System.out.println(response.body());
+                authFull = (JSONObject)JSONValue.parse(response.body()); //parse the response as JSON
+                auth = (String)authFull.get("token"); // update the auth token
+                authFull = null; //remove the JSON object
+                break;
+
+            default:
+                System.out.println("No such option for request.");
+                break;
+        }
+    }
 
 
     
