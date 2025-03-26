26aa3c8f61b8a657421296ee06ecb75672f3353b
dev-kit77 <christopher.r.cochrane@gmail.com>
2025-03-26 00:03:54 +0000

    Added JSON Dependency (Seriously where was this???)

--- src/module-info.java
+++ src/module-info.java
@@ -5,8 +5,9 @@ module PeblClient {
 	requires javafx.media;
 	requires javafx.controls;
 	requires javafx.web;
+	requires org.json;
 
-	opens app.pebl to javafx.graphics, javafx.fxml;
+	opens app.pebl to javafx.graphics, javafx.fxml, org.json;
 	opens app.pebl.friends to javafx.fxml;
 	opens app.pebl.profile to javafx.fxml;
 	opens app.pebl.login to javafx.fxml;
