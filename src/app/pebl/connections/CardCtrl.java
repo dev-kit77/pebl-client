f9a5395c7e7695e0fe4f7fe35f2d01fcc4686bba
dev-kit77 <christopher.r.cochrane@gmail.com>
2025-03-30 03:13:20 +0100

    Added basic profile card controller

--- src/app/pebl/connections/CardCtrl.java
+++ src/app/pebl/connections/CardCtrl.java
@@ -1,7 +1,27 @@
 package app.pebl.connections;
 
-import javafx.scene.layout.*; 
+import app.pebl.profile.User;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
 
 public class CardCtrl extends HBox {
-	
+	@FXML Label lblDisplayName;
+	@FXML Label lblUsername;
+	@FXML Label lblSkips;
+	@FXML Label lblStatus;
+
+	public void setUser(User displayUser) {
+		lblUsername.setText("@" + displayUser.getUsername());
+		//lblDisplayName.setText(displayUser.getDisplayName());
+		lblSkips.setText("(" + displayUser.getSkips() + " Skips)");
+		lblStatus.setText(displayUser.getStatus());
+	}
+
+	public void setUser(String username, String displayName,int skips, String status) {
+		lblUsername.setText("@" + username);
+		lblDisplayName.setText(displayName);
+		lblSkips.setText("(" + skips + " Skips)");
+		lblStatus.setText(status);
+	}
 }
