6acd806bd19f56463276c3747ce968b9def237f4
dev-kit77 <christopher.r.cochrane@gmail.com>
2025-03-30 04:01:46 +0100

    Added Basic Post controller

--- src/app/pebl/posts/PostCtrl.java
+++ src/app/pebl/posts/PostCtrl.java
@@ -1,7 +1,37 @@
 package app.pebl.posts;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.*;
+import javafx.scene.text.Text;
 
 public class PostCtrl extends AnchorPane {
-	
+	@FXML Label lblDisplayname;
+	@FXML Label lblUsername;
+	@FXML Text txtBody;
+	@FXML ToggleButton btnSkip;
+
+	public void toggleSkip() {
+		if (btnSkip.isSelected()) {
+			//add skip to post
+			System.out.println("add skip");
+		}
+		else {
+			//remove skip from post
+			System.out.println("remove skip");
+		}
+	}
+
+	public void setPost(Post post) {
+		//lblDisplayname.setText(post.getDisplayname);
+		lblUsername.setText(post.getSender());
+		txtBody.setText(post.getContent());
+	}
+
+	public void setPost(String displayname, String sender, String content) {
+		lblDisplayname.setText(displayname);
+		lblUsername.setText(sender);
+		txtBody.setText(content);
+	}
 }
