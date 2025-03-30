f60f5ca3b95705c583cf6b72c6094ca338ada6cf
dev-kit77 <christopher.r.cochrane@gmail.com>
2025-03-30 05:58:40 +0100

    Added skips field to posts

--- src/app/pebl/posts/PostCtrl.java
+++ src/app/pebl/posts/PostCtrl.java
@@ -9,27 +9,32 @@ import javafx.scene.text.Text;
 public class PostCtrl extends AnchorPane {
 	@FXML Label lblUsername;
 	@FXML Text txtBody;
+	@FXML Label lblSkips;
 	@FXML ToggleButton btnSkip;
 
 	public void toggleSkip() {
 		if (btnSkip.isSelected()) {
-			//add skip to post
+			//add skip to post and update label
 			System.out.println("add skip");
 		}
 		else {
-			//remove skip from post
+			//remove skip from post and update label
 			System.out.println("remove skip");
 		}
 	}
 
 	public void setPost(Post post) {
+		//update fields
 		lblUsername.setText(post.getSender());
 		txtBody.setText(post.getContent());
+		lblSkips.setText(Integer.toString(post.getSkips()));
 	}
 
-	public void setPost(String sender, String content) {
+	public void setPost(String sender, String content, int skips) {
+		//update fields
 		lblUsername.setText(sender);
 		txtBody.setText(content);
+		lblSkips.setText(Integer.toString(skips));
 	}
 
 
