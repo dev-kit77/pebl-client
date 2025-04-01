3eafb29b384ee63fe44b25c15074bf9878726deb
dev-kit77 <christopher.r.cochrane@gmail.com>
2025-04-01 03:50:22 +0100

    Updated leaderboard with new label and stopped bug of profiles changing text size

--- src/app/pebl/prompts/LeaderboardCtrl.java
+++ src/app/pebl/prompts/LeaderboardCtrl.java
@@ -9,18 +9,18 @@ import javafx.application.Platform;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class LeaderboardCtrl extends Controller {
-	@FXML Label lblCurrUsername;
-	@FXML Label lblCurrSkips;
-	@FXML Label lblCurrStatus;
+	@FXML Text lblCurrUsername;
+	@FXML Text lblCurrSkips;
+	@FXML Text lblCurrStatus;
 	@FXML VBox feedBoard;
 	@FXML MenuItem logout;
 
