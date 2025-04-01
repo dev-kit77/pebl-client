a9184223f5ea50aafd8c4e97df740bfd62aa1e46
dev-kit77 <christopher.r.cochrane@gmail.com>
2025-04-01 04:44:15 +0100

    Added Skip functionality to posts

--- src/app/pebl/posts/PostCtrl.java
+++ src/app/pebl/posts/PostCtrl.java
@@ -2,30 +2,131 @@ package app.pebl.posts;
 
 import app.pebl.Main;
 import app.pebl.data.Post;
+import app.pebl.util.Config;
+import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Separator;
 import javafx.scene.layout.*;
 import javafx.scene.text.Text;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class PostCtrl extends VBox {
 	@FXML Text lblUsername;
 	@FXML Text txtBody;
-	@FXML Text lblSkips;
 	@FXML Button btnSkip;
 	private Post post;
 
+	public void refresh() {
+		//get post data
+		Task<Void> postRefresh = new Task<>() {
+			@Override public Void call() {
+				try {
+					//get post from server
+					post = Main.getPost(post.getId());
+				} catch (Exception e) {
+					//print stack to console
+					e.printStackTrace();
+
+					//show error message
+					showError("Exception in pebl client", e.getMessage());
+
+					//exit app
+					Platform.exit();
+				}
+
+				//check that get succeeded
+				if (post != null) {
+					Platform.runLater(new Runnable() {
+						@Override
+						public void run() {
+							try {
+								//update fields
+								lblUsername.setText(post.getSender());
+								txtBody.setText(post.getContent());
+								btnSkip.setText(post.getSkips() + " Skips");
+
+								//disable button if post is from current user
+								if (post.getSender().equals(Config.getInstance().getCurrentUser().getUsername())) {
+									btnSkip.setDisable(true);
+								}
+								else{
+									btnSkip.setDisable(false);
+								}
+							} catch (Exception e) {
+								//print stack to console
+								e.printStackTrace();
+
+								//show error message
+								showError("Exception in pebl client", e.getMessage());
+
+								//exit app
+								Platform.exit();
+							}
+						}
+					});
+				}
+
+				//end task with null return
+				return null;
+			}
+		};
+
+		//run thread
+		Main.getExecutor().execute(postRefresh);
+	}
+
 	public void skip() {
-		//add skip to post and update label
-		System.out.println("add skip");
+		//get user data
+		Task<Void> skip = new Task<>() {
+			@Override public Void call() {
+				try {
+					//send like to server
+					Main.like(post.getId());
+				} catch (Exception e) {
+					//print stack to console
+					e.printStackTrace();
+
+					//show error message
+					showError("Exception in pebl client", e.getMessage());
+
+					//exit app
+					Platform.exit();
+				}
+
+				Platform.runLater(new Runnable() {
+					@Override
+					public void run() {
+						refresh();
+					}
+				});
+
+				//end task
+				return null;
+			}
+		};
+
+		//execute on thread
+		Main.getExecutor().execute(skip);
 	}
 
 	public void setPost(Post newPost) {
 		//update fields
 		lblUsername.setText(newPost.getSender());
 		txtBody.setText(newPost.getContent());
-		lblSkips.setText(newPost.getSkips() + " Skips");
+		btnSkip.setText(newPost.getSkips() + " Skips");
+
+		//disable button if post is from current user
+		if (newPost.getSender().equals(Config.getInstance().getCurrentUser().getUsername())) {
+			btnSkip.setDisable(true);
+		}
+		else{
+			btnSkip.setDisable(false);
+		}
 
 		//link post object
 		this.post = newPost;
@@ -35,11 +136,20 @@ public class PostCtrl extends VBox {
 		//update fields
 		lblUsername.setText(sender);
 		txtBody.setText(content);
-		lblSkips.setText(Integer.toString(skips));
+		btnSkip.setText(Integer.toString(skips));
 	}
 
 	public void showProfile() throws IOException {
 		//open new profile window with user
 		Main.initProfile(post.getSender()).show();
+	}
+
+	protected void showError(String header, String content) {
+		//create alert
+		Alert alert = new Alert(Alert.AlertType.ERROR);
+		alert.setTitle("Error");
+		alert.setHeaderText(header);
+		alert.setContentText(content);
+		alert.showAndWait();
 	}
 }
